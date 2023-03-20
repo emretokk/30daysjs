28e1457598526f1809ef5a81433efe43a1dce36b
emretokk <emretok35@gmail.com>
2023-03-21 00:27:07 +0300

    day 16 completed

---
+++ day16/code.js
@@ -0,0 +1,266 @@
+// Converting JSON to Object
+// JSON.parse()
+
+const usersText = `{
+"users":[
+  {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+  },
+  {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+  },
+  {
+  "firstName":"Lidiya",
+  "lastName":"Tekle",
+  "age":28,
+  "email":"lidiya@lidiya.com"
+  }
+]
+}`
+
+const usersObj = JSON.parse(usersText)
+console.log(usersObj)
+
+// using a reviver function with JSON.parse()
+const usersObj2 = JSON.parse(usersText, (anahtar, value) => {
+  let newValue =
+    typeof value == 'string' && anahtar != 'email' 
+    ? value.toUpperCase() : value
+  return newValue
+})
+console.log(usersObj2)
+
+
+
+
+
+
+// Converting Object to JSON
+// JSON.stringify()
+
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Redux',
+      'MongoDB',
+      'Express',
+      'React',
+      'Node'
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'MongoDB',
+      'Express',
+      'React',
+      'Node'
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
+
+
+//                         obj,   replacer,  space
+const txt = JSON.stringify(users, undefined, 4)
+console.log(txt) 
+// text means JSON- because json is 
+// a string form of an object.
+
+
+
+// Using a Filter Array with JSON.stringify()
+const user = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  country: 'Finland',
+  city: 'Helsinki',
+  email: 'alex@alex.com',
+  skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+  age: 250,
+  isLoggedIn: false,
+  points: 30
+}
+
+const text = JSON.stringify(user,['firstName', 'lastName', 'country', 'city', 'age'],4)
+console.log(text)
+
+// EXERCISES
+
+let isMarried = 250
+const teext = JSON.stringify(isMarried, undefined, 4)
+console.log(teext)
+
+
+const tex = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`
+console.clear()
+let mostSkill = 0
+let people = []
+let winner = ""
+const myJsonObj = JSON.parse(tex, (key, value) => {
+	if (key == "skills" && value.length > mostSkill) {
+		mostSkill = value.length
+		winner = key
+	}
+	return value
+})
+console.log(myJsonObj)
+console.log(mostSkill)
+
+for (person of Object.entries(myJsonObj)){
+	people.push(person)
+}
+
+for (person of people){
+	if(person[1].skills.length == mostSkill){
+		console.log(`Winner is: ${person[0]}`)
+	}
+}
