75889567401ed66398e5f222329204e235f8795a
emretokk <emretok35@gmail.com>
2023-03-23 15:34:28 +0300

    day 18-19 finished day 20 skipped

---
+++ day19/code.js
@@ -0,0 +1,37 @@
+// Closure
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
+
+
+function outerFunctionn() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunctionn()
+
+console.log(innerFuncs.minusOne)
+console.log(innerFuncs.plusOne)
